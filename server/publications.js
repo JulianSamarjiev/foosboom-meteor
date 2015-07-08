1bf99a33d56442a3ef8e3a0790ffedb9ff28462e
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-07-08 19:43:03 +0200

    figure 3.12: create button

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('teams', function() {
   return Teams.find();
 });
+
+Meteor.publish('games', function(){
+  return Games.find();
+});
--- server/seeds.js
+++ server/seeds.js
@@ -1,11 +1,40 @@
 Meteor.startup(function () {
   if (Teams.find().count() === 0) {
+    // Create some teams
     [
-    { name: "Barcelona" },
-    { name: "Real Madrid" },
-    { name: "Levski" }
-    ] .forEach(function(team) {
+      {
+        name: "Barcelona",
+        gameIds: []
+      },
+      {
+        name: "Real Madrid",
+        gameIds: []
+      },
+      {
+        name: "Levski",
+        gameIds: []
+      }
+    ].forEach(function(team) {
       Teams.insert(team);
     });
+
+    // Create a game
+    var team1 = Teams.find().fetch()[0];
+    var team2 = Teams.find().fetch()[1];
+
+    var game = {
+      completed: false,
+      createdAt: new Date(),
+      teams: [
+        {name: team1.name, _id: team1._id, score: 0},
+        {name: team2.name, _id: team2._id, score: 0}
+      ]
+    };
+
+    gameId = Games.insert(game);
+
+    // Add this game to both teams gameIds
+    Teams.update({_id: team1._id}, {$addToSet: { gameIds: gameId}});
+    Teams.update({_id: team2._id}, {$addToSet: { gameIds: gameId}})
   }
 });
